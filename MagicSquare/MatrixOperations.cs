5d75b6c6627b676ed14855baf4ac0fb06f1d1402
rootdobos <49080519+rootdobos@users.noreply.github.com>
2023-01-05 17:36:09 +0200

    backtracking with domainpointers

---
+++ MagicSquare/MatrixOperations.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicSquare
+{
+    public static class MatrixOperations
+    {
+        public static int[] SumInColumns(int[] variables, int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = SumInSingleColumn(variables,i,n);
+            }
+            return result;
+        }
+        public static int SumInSingleColumn(int[] variables, int i, int n)
+        {
+            int result = 0;
+            for (int p = 0; p < n; p++)
+            {
+                //if (variables[i + p * n] == -1)
+                //    return -1;
+                result+= variables[i + p * n];
+            }
+            return result;
+        }
+
+        public static int[] SumInRows(int[] variables, int n)
+        {
+            int[] result = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                result[i] = SumInSingleRow(variables, i, n);
+            }
+            return result;
+        }
+        public static int SumInSingleRow(int[] variables, int i, int n)
+        {
+            int result = 0;
+            for (int p = 0; p < n; p++)
+            {
+                result += variables[i *n+ p];
+            }
+            return result;
+        }
+        public static int SumInMainDiagonal(int[] variables, int n)
+        {
+            int result = 0;
+            for (int i = 0; i < n; i++)
+                result += variables[i + i * n];
+            return result;
+        }
+        public static int SumInAntiDiagonal(int[] variables, int n)
+        {
+            int result = 0;
+            for (int i = 0; i < n; i++)
+                result += variables[i + (n-(i+1)) * n];
+            return result;
+        }
+        public static bool IsEveryValueDifferent(int[] variables)
+        {
+            for(int i=0;i<variables.Length-1;i++)
+                for(int j=i+1;j<variables.Length;j++)
+                {
+                    if (variables[i] == variables[j])
+                        return false;
+                }
+
+            return true;
+        }
+    }
+}
--- MagicSquare/Program.cs
+++ MagicSquare/Program.cs
@@ -12,9 +12,9 @@ namespace MagicSquare
         {
             int n = 3;
 
-            int[] positions = new int[n * n];
-            for (int i = 0; i < positions.Length; i++)
-                positions[i] = -1;
+            int[] variables = new int[n * n];
+            for (int i = 0; i < variables.Length; i++)
+                variables[i] = i;
 
             int[][] domains = new int[n * n][];
             for(int i=0;i < domains.Length;i++)
@@ -23,7 +23,70 @@ namespace MagicSquare
                 for (int j = 0; j < domains[i].Length; j++)
                     domains[i][j] = j + 1;
             }
+            BackTracking(variables, domains, n);
+
             Console.ReadLine();
+        }
+
+        static void BackTracking(int[] variables, int[][] domains, int n)
+        {
+            int[][] localDomains = new int[n * n][];
+            for(int i=0;i<localDomains.Length;i++)
+            {
+                localDomains[i] = new int[n * n];
+                domains[i].CopyTo(localDomains[i], 0);
+            }
+
+            int variablePointer = 0;
+            int[] domainPointers = new int[n*n];
+            for (int i = 0; i < domainPointers.Length; i++)
+                domainPointers[i] = -1;
+
+            while(variablePointer!=-1)
+            {
+                if (domainPointers[variablePointer] < localDomains[variablePointer].Length-1)
+                {
+                    domainPointers[variablePointer]++;
+                    if(variablePointer==localDomains.Length-1)
+                    {
+                        if (CheckConstraints(variables, domains, domainPointers, n))
+                        {
+                            for(int c=0;c<n*n;c++)
+                            {
+                                Console.Write(variables[c] + " ");
+                            }
+                            Console.Write("\n");
+                        }
+                    }
+                    else
+                        variablePointer++;
+                }
+                else
+                {
+                    domainPointers[variablePointer] = -1;
+                    variablePointer--;
+                }
+            }
+        }
+        static bool CheckConstraints(int[] variables,int[][] domains, int[] domainPointers,int n)
+        {
+            for(int i=0;i< variables.Length; i++)
+            {
+                variables[i] = domains[i][domainPointers[i]];
+            }
+            if (!MatrixOperations.IsEveryValueDifferent(variables))
+                return false;
+            int[] sumInColumns = MatrixOperations.SumInColumns(variables, n);
+            int[] sumInRows = MatrixOperations.SumInRows(variables, n);
+            int sumInMainDiagonal = MatrixOperations.SumInMainDiagonal(variables, n);
+            int sumInAntiDiagonal = MatrixOperations.SumInAntiDiagonal(variables, n);
+
+            if (sumInMainDiagonal != sumInAntiDiagonal)
+                return false;
+            for (int i = 0; i < n; i++)
+                if (sumInColumns[i] != sumInMainDiagonal || sumInRows[i] != sumInMainDiagonal)
+                    return false;
+            return true;
         }
     }
 }
